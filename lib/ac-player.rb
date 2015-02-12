81765302939d034ddbfbf0d0439ac8e23426a229
Simon Aquino <simon.aquino@digital.cabinet-office.gov.uk>
2015-02-12 22:42:14 +0000

    Application refactoring
    Moved the steps to run the application into its own App class, within
    the run method. bin/ac-player now just instantiates the App class and
    calls the run method that takes care of anything else.

--- lib/ac-player.rb
+++ lib/ac-player.rb
@@ -1,2 +1,13 @@
 require 'crawler/login'
 require 'audienceclub/show_parser'
+
+class App
+
+  def run
+    login = Crawler::Login.new
+    shows_page = login.run
+    show_parser = AudienceClub::ShowParser.new(shows_page)
+    show_parser.build_shows
+  end
+
+end
