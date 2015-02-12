b8fc601e48e09210f09ac9bf444d19e96e69297b
Simon Aquino <simon.aquino@digital.cabinet-office.gov.uk>
2015-02-12 22:16:51 +0000

    Fixes #6: Skip multiple announce pages
    Sometimes the AC will insert multiple announce pages before landing on
    the show pages. It'd be nice to skip all the announce pages at once.
    The script now looks for a 'Click Here' button on the page and hits it
    until the show page appears.

--- lib/crawler/login.rb
+++ lib/crawler/login.rb
@@ -7,23 +7,42 @@ module Crawler
 
   class Login
 
+    def skip_announce_pages
+      begin
+        @current_page.forms.each do |form|
+          if form.button_with(:value => /Click Here/)
+            @current_page = form.click_button
+          else
+            raise "Exception: Can't find 'Click here' button to click in announce page"
+          end
+        end
+      end until @current_page.link_with(:text => /Theatre Ladder/)
+    end
+
     def do_login
       # Get a login page
-      login_page = Crawler.browser.get(Crawler::AC_LOGIN_URL)
+      @current_page = Crawler.browser.get(Crawler::AC_LOGIN_URL)
 
       # Get a login form
-      login_form = login_page.form_with(:name => /login/)
+      login_form = @current_page.form_with(:name => /login/)
 
       # Set the login fields
       login_form.field_with(:type => /email/).value = Crawler::EMAIL
       login_form.field_with(:type => /password/).value = Crawler::PASSWD
 
-      # Hit submit
-      welcome_page = login_form.click_button
+      # Hit submit and return next page
+      @current_page = login_form.click_button
 
-      # Click continue
-      return welcome_page.form.click_button
     end
+
+    def run
+      do_login
+      skip_announce_pages
+
+      @current_page
+    end
+
+  private :do_login, :skip_announce_pages
 
   end
 
